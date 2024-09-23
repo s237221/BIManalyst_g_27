dc4422ef1c49323b06c46961fdd5c90f92cf74e3
s237221 <s237221@dtu.dk>
2024-09-23 17:57:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import ifcopenshell
 from ifcopenshell.util import element
 
 from rules import column_AreaRule
-#from .rules import windowRule
-#from .rules import doorRule
-
 model = ifcopenshell.open("C:/Users/Alexandra/Desktop/SCHOOL/DTU Education/SEM1/Advanced BIM/LECTURE 1/CES_BLD_24_06_STR.ifc")
-
-#windowResult = windowRule.checkRule(model)
-#doorResult = doorRule.checkRule(model)
 columnResult = column_AreaRule.checkRule(model)
-
-#print("Window result:", windowResult)
-#print("Door result:", doorResult)
 print("Column result:", columnResult)
