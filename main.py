9e19badf9df74ed58cd7ff63012a29c5e1ce025c
s237221 <s237221@dtu.dk>
2024-11-04 15:34:24 +0100

    All works now

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import ifcopenshell
 from ifcopenshell.util import element
 
-from rules import column_checkReportRule
+from rules import try_out_A3_WORKS
 model = ifcopenshell.open("C:/Users/Alexandra/Desktop/SCHOOL/DTU_Education/SEM1/Advanced_BIM/LECTURE 1/CES_BLD_24_06_STR.ifc")
-columnResult = column_checkReportRule.checkRule(model)
+columnResult = column_try_out_A3_WORKS.checkRule(model,10,360,360)
 print("Column result:", columnResult)
 
 
