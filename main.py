84ad8400d0144b63c3e4b0c198ef2a00fd377713
s237221 <s237221@dtu.dk>
2024-11-04 15:16:29 +0100

    A3

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import ifcopenshell
 from ifcopenshell.util import element
 
-from rules import column_AreaRule
-model = ifcopenshell.open("C:/Users/Alexandra/Desktop/SCHOOL/DTU Education/SEM1/Advanced BIM/LECTURE 1/CES_BLD_24_06_STR.ifc")
-columnResult = column_AreaRule.checkRule(model)
+from rules import column_checkReportRule
+model = ifcopenshell.open("C:/Users/Alexandra/Desktop/SCHOOL/DTU_Education/SEM1/Advanced_BIM/LECTURE 1/CES_BLD_24_06_STR.ifc")
+columnResult = column_checkReportRule.checkRule(model)
 print("Column result:", columnResult)
+
+
+
